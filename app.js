3eef95b5bed9f89e0989902ee3475d28f9f8fbd6
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-04-30 19:04:43 +0300

    feat: add cors

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const mainRoute = require("./routes/main");
 const gamesRouter = require("./routes/games");
+const { cors } = require("./middlewares/cors");
 
 const app = express();
 const PORT = 3000;
 
 app.use(
+    cors,
     bodyParser.json(),
     express.static(path.join(__dirname, "public")),
     mainRoute,
